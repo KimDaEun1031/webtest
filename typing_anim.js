a85d6f33bb897ee8a20928da0e4ea7d929fd8041
김다은 <73865700+KimDaEun1031@users.noreply.github.com>
2021-11-12 12:28:06 +0900

    Add animation

---
+++ typing_anim.js
@@ -0,0 +1,13 @@
+const content = "Hello, I'm Daeun";
+const text = document.querySelector(".text");
+let i = 0;
+
+function typing() {
+    if (i < content.length) {
+        let txt = content.charAt(i);
+        text.innerHTML += txt;
+        i++;
+    }
+}
+
+setInterval(typing, 200);
